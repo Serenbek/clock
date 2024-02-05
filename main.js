2a5d406be06f64c00f77d0506f191c4eb4d905f8
Serenbek Ibraimkulov <serenbek@mail.ru>
2024-02-05 15:12:28 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+let hour = document.querySelector("#hour");
+let minute = document.querySelector("#minute");
+let second = document.querySelector("#second");
+
+setInterval(() => {
+  date = new Date();
+  hr = date.getHours();
+  min = date.getMinutes();
+  sec = date.getSeconds();
+  hr_rotation = 30 * hr + min / 2;
+  min_rotation = 6 * min;
+  sec_rotation = 6 * sec;
+
+  hour.style.transform = `rotate(${hr_rotation}deg)`;
+  minute.style.transform = `rotate(${min_rotation}deg)`;
+  second.style.transform = `rotate(${sec_rotation}deg)`;
+}, 1000);
